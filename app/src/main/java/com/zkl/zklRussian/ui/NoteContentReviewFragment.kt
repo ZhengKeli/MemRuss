a7b799204c8178f774272b29e9116ed2daa50318
ZhengKeli <zhengkeli2009@126.com>
2017-10-27 17:59:27 +0700

    写了一些review部分

---
+++ app/src/main/java/com/zkl/zklRussian/ui/NoteContentReviewFragment.kt
@@ -0,0 +1,5 @@
+package com.zkl.zklRussian.ui
+
+import android.support.v4.app.Fragment
+
+abstract class NoteContentReviewFragment: Fragment()
--- app/src/main/java/com/zkl/zklRussian/ui/NoteReviewFragment.kt
+++ app/src/main/java/com/zkl/zklRussian/ui/NoteReviewFragment.kt
@@ -1,5 +1,10 @@
 package com.zkl.zklRussian.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zkl.zklRussian.R
 import com.zkl.zklRussian.control.note.NotebookKey
 
 class NoteReviewFragment: NoteHoldingFragment {
@@ -7,5 +12,14 @@ class NoteReviewFragment: NoteHoldingFragment {
 	constructor(notebookKey: NotebookKey, noteId: Long) : super(notebookKey,noteId)
 	
 	//todo
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
+		= inflater.inflate(R.layout.fragment_note_review, container, false).apply {
+		
+	}
+	
+	
+	
 	
 }
+
+
