f5f31caceb157f8dc0518424d452e545e620297f
ZhengKeli <zhengkeli2009@126.com>
2017-11-19 00:10:04 +0700

    改了删除对话框是否popBackStack的机制

--- app/src/main/java/com/zkl/zklRussian/ui/NoteDeleteDialog.kt
+++ app/src/main/java/com/zkl/zklRussian/ui/NoteDeleteDialog.kt
@@ -5,14 +5,19 @@ import android.app.Dialog
 import android.os.Bundle
 import com.zkl.zklRussian.R
 import com.zkl.zklRussian.control.note.NotebookKey
+import org.jetbrains.anko.bundleOf
 
 class NoteDeleteDialog:NoteHoldingDialog(){
 	
 	companion object {
-		fun newInstance(notebookKey: NotebookKey, noteId: Long)
-			= NoteDeleteDialog::class.java.newInstance(notebookKey, noteId)
+		private val arg_popBackStack = "popBackStack"
+		fun newInstance(notebookKey: NotebookKey, noteId: Long,popBackStack: Boolean=true)
+			= NoteDeleteDialog::class.java.newInstance(notebookKey, noteId).apply {
+			arguments+= bundleOf(arg_popBackStack to popBackStack)
+		}
 	}
 	
+	val popBackStack:Boolean get() = arguments.getBoolean(arg_popBackStack)
 	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 		return AlertDialog.Builder(context)
 			.setTitle(R.string.confirm_deletion)
@@ -20,6 +25,7 @@ class NoteDeleteDialog:NoteHoldingDialog(){
 			.setNegativeButton(android.R.string.cancel,null)
 			.setPositiveButton(android.R.string.ok,{ _, _ ->
 				mutableNotebook.deleteNote(noteId)
+				if(popBackStack) fragmentManager.popBackStack()
 			})
 			.create()
 	}
