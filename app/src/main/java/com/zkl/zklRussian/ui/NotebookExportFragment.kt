c4e71dfc6824a4b0036f636698441472a79e42d6
ZhengKeli <zhengkeli2009@126.com>
2017-11-22 13:51:24 +0700

    开始写NotebookExportFragment

---
+++ app/src/main/java/com/zkl/zklRussian/ui/NotebookExportFragment.kt
@@ -0,0 +1,17 @@
+package com.zkl.zklRussian.ui
+
+import android.support.v4.app.Fragment
+import com.zkl.zklRussian.control.note.NotebookShelf
+import org.jetbrains.anko.bundleOf
+
+class NotebookExportFragment:Fragment(){
+	
+	companion object {
+		val arg_notebookSummary="notebookSummary"
+		fun newInstance(notebookSummary: NotebookShelf.NotebookSummary)
+			= NotebookExportFragment::class.java.newInstance().apply {
+			arguments+= bundleOf(arg_notebookSummary to notebookSummary)
+		}
+	}
+	
+}
--- app/src/main/java/com/zkl/zklRussian/ui/NotebookMenuDialog.kt
+++ app/src/main/java/com/zkl/zklRussian/ui/NotebookMenuDialog.kt
@@ -2,22 +2,29 @@ package com.zkl.zklRussian.ui
 
 import android.app.Dialog
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import com.zkl.zklRussian.R
-import com.zkl.zklRussian.control.note.NotebookKey
+import com.zkl.zklRussian.control.note.NotebookShelf
+import org.jetbrains.anko.bundleOf
 
-class NotebookMenuDialog : NotebookHoldingDialog() {
+class NotebookMenuDialog:DialogFragment() {
 	
 	companion object {
-		fun newInstance(notebookKey: NotebookKey)
-			= NotebookMenuDialog::class.java.newInstance(notebookKey)
+		val arg_notebookSummary="notebookSummary"
+		fun newInstance(notebookSummary: NotebookShelf.NotebookSummary)
+			= NotebookMenuDialog::class.java.newInstance().apply {
+			arguments+= bundleOf(arg_notebookSummary to notebookSummary)
+		}
 	}
+	
+	val notebookSummary get() = arguments.getSerializable(arg_notebookSummary) as NotebookShelf.NotebookSummary
 	
 	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 		
 		val itemPairs = arrayOf(
 			getString(R.string.export_Notebook) to {
-				//todo open export dialog
+				NotebookExportFragment.newInstance(notebookSummary).jump(fragmentManager,true)
 			}
 		)
 		val itemNames = itemPairs.map { it.first }.toTypedArray<String>()
