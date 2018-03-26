5f0e4e6d1894042e5813bf28b38c88944b24e8ad
ZhengKeli <zhengkeli2009@126.com>
2018-03-26 11:44:44 +0700

    reimplemented threading with a simpler structure (not tested)

--- app/src/main/java/com/zkl/memruss/ui/NotebookSearchFragment.kt
+++ app/src/main/java/com/zkl/memruss/ui/NotebookSearchFragment.kt
@@ -13,7 +13,6 @@ import com.zkl.memruss.core.note.Note
 import kotlinx.android.synthetic.main.fragment_notebook_search.*
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.channels.ConflatedChannel
 
 class NotebookSearchFragment : NotebookHoldingFragment() {
 	
@@ -35,7 +34,7 @@ class NotebookSearchFragment : NotebookHoldingFragment() {
 		sv_search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 			override fun onQueryTextSubmit(query: String): Boolean = true
 			override fun onQueryTextChange(newText: String): Boolean {
-				startSearch(newText.trim())
+				search(newText.trim())
 				return true
 			}
 		})
@@ -54,54 +53,50 @@ class NotebookSearchFragment : NotebookHoldingFragment() {
 	
 	
 	//search
-	data class Search(val text: String, val deferredResult: Deferred<List<Note>>)
-	
-	private val searchChan = ConflatedChannel<Search>().apply {
-		launch(CommonPool) {
-			while (isActive) {
-				val (text, deferredResult) = receiveOrNull() ?: break
-				val result = deferredResult.await()
-				launch(UI) { resultStart(text, result) }
+	private var searchJob: Job? = null
+	private var searchResult: List<Note> = emptyList()
+	private fun search(searchText: String) {
+		val oldJob = searchJob
+		searchJob = launch(CommonPool) {
+			oldJob?.cancelAndJoin()
+			val searchingUI = launch(UI + coroutineContext) ui@{
+				if (!isVisible) return@ui
+				lv_notes.isEnabled = false
+				
+				delay(500)
+				
+				if (!isVisible) return@ui
+				pb_searching.visibility = View.VISIBLE
+			}
+			
+			val result =
+				if (searchText.isEmpty()) emptyList<Note>()
+				else async {
+					delay(100)
+					notebook.selectByKeyword(searchText)
+				}.await()
+			
+			searchingUI.cancel()
+			launch(UI + coroutineContext) ui@{
+				if (!isVisible) return@ui
+				
+				lv_notes.isEnabled = true
+				pb_searching.visibility = View.GONE
+				
+				if (result.isEmpty()) {
+					tv_foundNothing.visibility = View.VISIBLE
+					tv_foundNothing.text =
+						if (searchText.isEmpty()) getString(R.string.type_to_search)
+						else getString(R.string.found_nothing)
+				} else {
+					tv_foundNothing.visibility = View.GONE
+				}
+				
+				searchResult = result
+				(lv_notes.adapter as BaseAdapter).notifyDataSetChanged()
 			}
 		}
 	}
-	
-	private var searchResult: List<Note> = emptyList()
-	
-	private fun startSearch(searchText: String) {
-		val deferredResult = async(CommonPool, CoroutineStart.LAZY) {
-			if (searchText.isEmpty()) return@async emptyList<Note>()
-			delay(100)
-			notebook.selectByKeyword(searchText)
-		}
-		launch(CommonPool) { searchChan.send(Search(searchText, deferredResult)) }
-		lv_notes.isEnabled = false
-		launch(UI) {
-			delay(500)
-			if (!this@NotebookSearchFragment.isVisible) return@launch
-			if (deferredResult.isActive) pb_searching.visibility = View.VISIBLE
-		}
-	}
-	
-	private fun resultStart(searchText: String, result: List<Note>) {
-		if (!this@NotebookSearchFragment.isVisible) return
-		
-		lv_notes.isEnabled = true
-		pb_searching.visibility = View.GONE
-		
-		if (result.isEmpty()) {
-			tv_foundNothing.visibility = View.VISIBLE
-			tv_foundNothing.text =
-				if (searchText.isEmpty()) getString(R.string.type_to_search)
-				else getString(R.string.found_nothing)
-		} else {
-			tv_foundNothing.visibility = View.GONE
-		}
-		
-		searchResult = result
-		(lv_notes.adapter as BaseAdapter).notifyDataSetChanged()
-	}
-	
 	
 }
 
