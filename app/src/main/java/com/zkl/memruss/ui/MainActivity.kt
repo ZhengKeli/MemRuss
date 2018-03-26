f34e797378974ef8bbdf8be60e42aaff709152aa
ZhengKeli <zhengkeli2009@126.com>
2018-03-26 11:43:19 +0700

    optimized dependencies

--- app/src/main/java/com/zkl/memruss/ui/MainActivity.kt
+++ app/src/main/java/com/zkl/memruss/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zkl.memruss.ui
 
 import android.app.UiModeManager
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
@@ -10,7 +11,6 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import com.zkl.memruss.R
-import org.jetbrains.anko.uiModeManager
 import java.util.*
 
 
@@ -19,12 +19,12 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		if (savedInstanceState == null) {
-			val calendar = Calendar.getInstance()
-			val hour = calendar.get(Calendar.HOUR_OF_DAY)
-			uiModeManager.nightMode = when (hour) {
-				in 0..7 -> UiModeManager.MODE_NIGHT_YES
-				in 19..23 -> UiModeManager.MODE_NIGHT_YES
-				else -> UiModeManager.MODE_NIGHT_NO
+			(getSystemService(Context.UI_MODE_SERVICE) as UiModeManager).run {
+				nightMode = when (Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+					in 0..7 -> UiModeManager.MODE_NIGHT_YES
+					in 19..23 -> UiModeManager.MODE_NIGHT_YES
+					else -> UiModeManager.MODE_NIGHT_NO
+				}
 			}
 		}
 		setContentView(R.layout.activity_main)
@@ -43,6 +43,7 @@ class MainActivity : AppCompatActivity() {
 	interface BackPressedHandler {
 		fun onBackPressed(): Boolean
 	}
+	
 	override fun onBackPressed() {
 		val backHandler = getShowingFragment() as? BackPressedHandler
 		val absorbed = backHandler?.onBackPressed() == true
@@ -52,10 +53,11 @@ class MainActivity : AppCompatActivity() {
 	
 	//permissions request
 	private val requestPermissionCallbacks = AutoIndexMap<RequestPermissionCallback>()
+	
 	fun requestPermission(permission: String, force: Boolean = false, callback: RequestPermissionCallback) {
-		if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED){
+		if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
 			callback(true, true)
-		}else {
+		} else {
 			if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission) && !force) {
 				callback(false, true)
 			} else {
@@ -64,6 +66,7 @@ class MainActivity : AppCompatActivity() {
 			}
 		}
 	}
+	
 	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
 		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 		requestPermissionCallbacks.remove(requestCode)?.invoke(
@@ -78,12 +81,12 @@ val Fragment.mainActivity get() = activity as MainActivity
 
 
 //fragment jumping
-fun Fragment.jump(fragmentManager: FragmentManager, addToBackStack: Boolean = true, animate:Boolean = addToBackStack) {
+fun Fragment.jump(fragmentManager: FragmentManager, addToBackStack: Boolean = true, animate: Boolean = addToBackStack) {
 	val transaction = fragmentManager.beginTransaction()
 	if (animate) transaction.setCustomAnimations(
 		R.animator.fly_in_right, R.animator.fly_out_left,
 		R.animator.fly_in_left, R.animator.fly_out_right)
-	else if(addToBackStack && !animate){
+	else if (addToBackStack && !animate) {
 		transaction.setCustomAnimations(0, 0,
 			R.animator.fly_in_left, R.animator.fly_out_right)
 	}
@@ -91,15 +94,17 @@ fun Fragment.jump(fragmentManager: FragmentManager, addToBackStack: Boolean = tr
 	if (addToBackStack) transaction.addToBackStack(null)
 	transaction.commit()
 }
-fun Fragment.jumpFade(fragmentManager: FragmentManager,addToBackStack: Boolean=true){
+
+fun Fragment.jumpFade(fragmentManager: FragmentManager, addToBackStack: Boolean = true) {
 	val transaction = fragmentManager.beginTransaction()
 	transaction.setCustomAnimations(R.animator.fade_in, R.animator.fade_out, R.animator.fade_in, R.animator.fade_out)
 	transaction.replace(R.id.fragment_container, this)
 	if (addToBackStack) transaction.addToBackStack(null)
 	transaction.commit()
 }
-fun DialogFragment.show(fragmentManager: FragmentManager){
-	show(fragmentManager,null)
+
+fun DialogFragment.show(fragmentManager: FragmentManager) {
+	show(fragmentManager, null)
 }
 
 
